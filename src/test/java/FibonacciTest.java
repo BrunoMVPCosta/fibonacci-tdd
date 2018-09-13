30f1d1f56a960ac3f7ec5394c00648a7bfaa1b0c
Bruno Costa <bcosta10@gmail.com>
2018-09-13 22:12:58 +0100

    Add second test

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FibonacciTest {
-    @Test public void fibonacci_given0_returns0() {
+    @Test 
+    public void fibonacci_given0_returns0() {
         // arrange
         Fibonacci fibonacci = new Fibonacci();
         int number = 0;
@@ -16,4 +17,18 @@ public class FibonacciTest {
         // assert
         assertSame(result, 0);
     }
+
+    @Test 
+    public void fibonacci_given1_returns1() {
+        // arrange
+        Fibonacci fibonacci = new Fibonacci();
+        int number = 1;
+
+        // act
+        int result = fibonacci.calculate(number);
+
+        // assert
+        assertSame(result, 1);
+    }
+
 }
